243913c3466a2a1094ec85611540538f90cc68e5
FuneralSuits18 <mahirtahmid@gmail.com>
2024-05-10 21:34:12 +0600

    Touch up

--- update.sh
+++ update.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-# 1. Make it executable: 'chmod +x update.sh'
-# 2. Move it to PATH: 'sudo mv update.sh /usr/local/bin/update'
-# 3. Test: run 'sudo update' and see if it works
-
-# Check if the user has sudo privileges
-if [ "$(id -u)" != "0" ]; then
-    echo "This script must be run with sudo." 1>&2
-    exit 1
-fi
-
-# Perform package updates
-dnf update && flatpak update
+sudo dnf update && flatpak update
